4db21a974fde628d26cd67752a9b9a33ffffb1a0
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-11-05 22:00:25 +0100

    Update Gif.js component

--- components/Gif.js
+++ components/Gif.js
@@ -1,9 +1,12 @@
 
 class Gif extends React.Component {
   render() {
+    //var url = gif.fixed_width_downsampled_url
     return (
         <div>
-          <img id='gif' src={this.props.gif.url} className={'gif_image'}/>
+        <a href={this.props.gif.url} title='viev this on giphy' target='new'>
+          <div id='gif' src={this.props.gif.embed_url} className={'gif_image'}> </div>
+        </a>
         </div>
     );
   }
