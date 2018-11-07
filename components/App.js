ccb28a7eb715d51bbe95ed94c1d8cb943f6a0511
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-11-07 17:13:55 +0100

    Add new methods and update content for App.js component

--- components/App.js
+++ components/App.js
@@ -4,29 +4,31 @@ class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        value: '',
         gif: {},
         gifUrl: '',
         sourceUrl: '',
+        animal: '',
         gifLoaded: false
       };
     }
 
-    searchAnimals() {
-      fetch('https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=animals')
-        .then(response => response.json())
-        .then(responseJson => this.setState({
-            gif: {
-              gifUrl: responseJson.data.fixed_height_downsampled_url,
-              sourceUrl: responseJson.data.url
-            }
-          }
-        )
-      );
+    getRandomAnimal() {
+        var possibleAnimals = ['cat', 'dog', 'monkey', 'elephant', 'lion', 'bear', 'bird', 'mouse', 'squirrel'];
+        return possibleAnimals[Math.floor(Math.random()*9)];
     }
-    searchCats(event) {
+
+    setAnimal() {
+       var searchAnimal = this.getRandomAnimal();
+       this.setState({
+         animal: searchAnimal
+       });
+    }
+
+    searchAnimals(event) {
+      this.setAnimal();
+      const {animal} = this.state;
       event.preventDefault();
-      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=cat`;
+      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=funny+${animal}`;
       fetch(url)
         .then(response => response.json())
         .then(responseJson => this.setState({
@@ -36,13 +38,12 @@ class App extends React.Component {
             },
             gifLoaded: true
           }
-        )
-      );
+        ));
     }
 
     searchDogs(event) {
       event.preventDefault();
-      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=dog`;
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=dog`;
       fetch(url)
         .then(response => response.json())
         .then(responseJson => this.setState({
@@ -51,13 +52,28 @@ class App extends React.Component {
               sourceUrl: responseJson.data.url
             },
             gifLoaded: true
-          })
-      );
+          }
+        ));
+    }
+
+    searchCats(event) {
+      event.preventDefault();
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=cat`;
+      fetch(url)
+        .then(response => response.json())
+        .then(responseJson => this.setState({
+            gif: {
+              gifUrl: responseJson.data.fixed_height_downsampled_url,
+              sourceUrl: responseJson.data.url
+            },
+            gifLoaded: true
+          }
+        ));
     }
 
     searchMonkeys(event) {
       event.preventDefault();
-      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=monkey`;
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=monkey`;
       fetch(url)
         .then(response => response.json())
         .then(responseJson => this.setState({
@@ -66,13 +82,13 @@ class App extends React.Component {
               sourceUrl: responseJson.data.url
             },
             gifLoaded: true
-          })
-      );
+          }
+        ));
     }
 
     searchElephants(event) {
       event.preventDefault();
-      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=elephant`;
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=elephant`;
       fetch(url)
         .then(response => response.json())
         .then(responseJson => this.setState({
@@ -81,13 +97,13 @@ class App extends React.Component {
             sourceUrl: responseJson.data.url
           },
           gifloaded: true
-        })
-      );
+          }
+        ));
     }
 
     searchLions(event) {
       event.preventDefault();
-      const url = `https://api.giphy.com/v1/stickers/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=lion`;
+      const url = `https://api.giphy.com/v1/gifs/random?api_key=JCNdIIUzjei776ICXPlx1V3EXQAcsWkC&tag=lion`;
       fetch(url)
         .then(response => response.json())
         .then(responseJson => this.setState({
@@ -99,60 +115,55 @@ class App extends React.Component {
         })
       );
     }
-    getGifs(gifLoaded) {
-      if (gifLoaded == false) {
-       this.searchAnimals();
-     }
-   }
     render() {
       return (
         <div>
           <h1> Animals&#39; gifs search engine</h1>
-          <h2> Search animals&#39; gifs by name</h2>
+          <h2> Search gifs by animal&#39; name</h2>
           <div className={'panel_title'}>
-              <h3> Dogs</h3>
-              <h3>Monkeys</h3>
-              <h3>Cats</h3>
-              <h3> Lions</h3>
-              <h3> Elephants</h3>
+              <h3>Dog</h3>
+              <h3>Monkey</h3>
+              <h3>Cat</h3>
+              <h3>Lion</h3>
+              <h3>Elephant</h3>
+              <h3>Surprise</h3>
           </div>
           <div className={'panel_buttons'}>
               <button
-                value="dogs"
                 id="dogs"
                 onClick ={(event)=>this.searchDogs(event)}>
                   <img src="images/Dogs.png"/>
               </button>
               <button
-                value="monkeys"
                 id="monkeys"
                 onClick ={(event)=>this.searchMonkeys(event)}>
                   <img src="images/Monkeys.png"/>
               </button>
               <button
-                value="cats"
                 id="cats"
                 onClick ={(event)=>this.searchCats(event)}>
                   <img src="images/Cats.png"/>
               </button>
               <button
-                value="lions"
                 id="lions"
                 onClick ={(event)=>this.searchLions(event)}>
                   <img src="images/Lions.png"/>
               </button>
               <button
-                value="elephants"
                 id="elephants"
                 onClick ={(event)=>this.searchElephants(event)}>
                   <img src="images/Elephants.png"/>
+              </button>
+              <button
+                id="elephants"
+                onClick ={(event)=>this.searchAnimals(event)}>
+                  <img src="images/Surprise.png"/>
               </button>
           </div>
           <Gif
               gifUrl={this.state.gif.gifUrl}
               sourceUrl={this.state.gif.sourceUrl}
               gifLoaded={this.state.gifLoaded}
-              getGifs ={this.getGifs()}
             />
       </div>
       );
