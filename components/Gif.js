03fda63ffd85d90c2f02577d46ff2dbf3b9b2d0c
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-11-07 17:14:22 +0100

    Update content for Gif.js component

--- components/Gif.js
+++ components/Gif.js
@@ -1,12 +1,19 @@
 
-
+var initial_gif_URL = "http://www.animatedimages.org/data/media/132/animated-animal-image-0057.gif"
+var styles = {
+  minWidth: '568px',
+  minHeight: '400px'
+}
 class Gif extends React.Component {
-
+  getUrl() {
+    return this.props.sourceUrl || initial_gif_URL;
+  }
   render() {
+    var url = !this.props.gifLoaded ? initial_gif_URL : this.props.gifUrl;
     return (
-        <div className={'gif'}>
-            <a href={this.props.sourceUrl} title='Viev this on giphy' target='new'>
-                <img id='gif' src={this.props.gifUrl} className={'gif_image'} />
+        <div className={'gif'} style={styles}>
+            <a href={this.getUrl()} title='Viev this on giphy' target='new'>
+                <img id='gif' src={url} className={'gif_image'} />
             </a>
         </div>
     );
